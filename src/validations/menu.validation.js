33912a83d1f63db1a5cd4f841b45e71e16e33f8c
Daffa SH <shidqidaffa@gmail.com>
2024-06-15 15:20:41 +0700

    feat: add uuid validation in menu

--- src/validations/menu.validation.js
+++ src/validations/menu.validation.js
@@ -4,6 +4,6 @@ export const upsertMenuSchema = Joi.object({
     name: Joi.string().required(),
     description: Joi.string().required(),
     price: Joi.number().required(),
-    menu_type_id: Joi.string().required(),
+    menu_type_id: Joi.string().uuid().required(),
     menu_status_id: Joi.number().required(),
 });
